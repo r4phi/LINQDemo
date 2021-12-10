2a9c5663d35b61c4ab3d1121f410a8a992719a75
raphi <raphi.kapeller@gmx.at>
2021-12-10 09:47:14 +0100

    Änderungen

---
+++ LINQDemo/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQDemo
+{
+    class Person
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int Age { get; set; }
+
+        public static IList<Person> Factory() //Factory Methoden sollen Daten vorbereiten; erzeugen
+        {
+            IList<Person> personList = new List<Person>();
+
+            personList.Add(new Person
+            {
+                FirstName = "Sue",
+                LastName = "Permarkt",
+                Age = 20
+            });
+
+            personList.Add(new Person
+            {
+                FirstName = "Luci",
+                LastName = "Fer",
+                Age = 666
+            });
+
+            personList.Add(new Person
+            {
+                FirstName = "Harri",
+                LastName = "Gersak",
+                Age = 69
+            });
+
+            personList.Add(new Person
+            {
+                FirstName = "Andi",
+                LastName = "Arbeit",
+                Age = 69
+            });
+
+            return personList;
+        }
+    }
+}
--- LINQDemo/Program.cs
+++ LINQDemo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace LINQDemo
 {
@@ -6,7 +8,64 @@ namespace LINQDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 1; //explizit typisiert
+            var z = 22; //implizit typisiert -> Wertzuweisung benötigt
+
+            //geht nicht - c# ist typsicher
+            // x = "xx";
+            //z = "xx";
+            //nicht typsierte Sprachen: JS, PHP -> kann zu laufzeit fehlern führen
+
+            IList<string> names = new List<string>();
+            names.Add("Mike Rhosoft");
+            names.Add("Sergey Fährlich");
+            names.Add("Karl Toffel");
+
+            //LINQ statememt
+            //liste wird nicht umsortiert, ist nur ein View
+            var sortedNames = from name in names
+                              where name.Length > 5
+                              orderby name descending
+                              select name;
+            foreach(var name in sortedNames)
+            {
+                Console.WriteLine(name);
+            }
+
+            //Lambda Expression
+            var sortedNames2 = names.Where(n => n.Length > 5).OrderByDescending(n => n);
+            foreach (var name in sortedNames2)
+            {
+                Console.WriteLine(name);
+            }
+
+            //Eigene Klasse
+            Console.WriteLine("\n----Eigene Klasse-----");
+            var sortedPerson = from p in Person.Factory()
+                               where p.FirstName.StartsWith('A')
+                orderby p.FirstName descending 
+                orderby p.LastName ascending 
+                               select p;
+
+            var sortedPerson2 = Person.Factory()
+                .Where(p => p.FirstName.StartsWith("A"))
+                .OrderByDescending(p => p.FirstName)
+                .ThenBy(p => p.LastName); //bei mehreren OrderBy nacheinander muss ThenBy verwendet werden
+
+            //Gruppierung
+            var groupedPersons = from p in Person.Factory()
+                group p by p.Age;
+
+            //Ausgabe der Gruppierung
+            foreach (var personGroup in groupedPersons)
+            {
+                Console.WriteLine($"Alter: {personGroup.Key}; Anzahl: {personGroup.Count()}");
+
+                foreach (var person in personGroup)
+                {
+                    Console.WriteLine($"{person.FirstName} {person.LastName}");
+                }
+            }
         }
     }
 }
